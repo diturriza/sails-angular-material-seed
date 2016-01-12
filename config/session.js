ae5dcb58f523551e3c9820507bb8c7e3a58957c7
Ronald Suez <rsuez93@gmail.com>
2016-01-12 14:52:30 -0430

    trying to make session work

--- config/session.js
+++ config/session.js
@@ -43,6 +43,16 @@ module.exports.session = {
 
   // adapter: 'redis',
 
+   cookie: {
+        maxAge: 60 * 60 * 24 * 7
+    },
+    adapter: 'redis',
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+    ttl: 60 * 60 * 24 * 7,
+    db: null,
+    pass: process.env.REDIS_PASS || null,
+    prefix: 'sess:'
   /***************************************************************************
   *                                                                          *
   * The following values are optional, if no options are set a redis         *
